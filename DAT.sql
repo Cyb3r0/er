82014c0133921b2dce51d6b2fdcb8fce90eb0c21
3Li <Cyb3r0@users.noreply.github.com>
2025-06-07 10:16:45 +0300

    Add files via upload

---
+++ DAT.sql
@@ -0,0 +1,50 @@
+
+Insert into  DAT
+   (ID, NO)
+ Values
+   (1, 31);
+Insert into  DAT
+   (ID, NO)
+ Values
+   (2, 28);
+Insert into  DAT
+   (ID, NO)
+ Values
+   (3, 31);
+Insert into  DAT
+   (ID, NO)
+ Values
+   (4, 30);
+Insert into  DAT
+   (ID, NO)
+ Values
+   (5, 31);
+Insert into  DAT
+   (ID, NO)
+ Values
+   (6, 30);
+Insert into  DAT
+   (ID, NO)
+ Values
+   (7, 31);
+Insert into  DAT
+   (ID, NO)
+ Values
+   (8, 31);
+Insert into  DAT
+   (ID, NO)
+ Values
+   (9, 30);
+Insert into  DAT
+   (ID, NO)
+ Values
+   (10, 31);
+Insert into  DAT
+   (ID, NO)
+ Values
+   (11, 30);
+Insert into  DAT
+   (ID, NO)
+ Values
+   (12, 31);
+COMMIT;
